4c850c1c89ae7c60295d668ca462e36ad98bb818
Nguyễn Tấn Trung <nguyentantrung3006@gmail.com>
2019-12-22 18:24:12 +0700

    sua file detail

--- resources/views/frontend/detail.blade.php
+++ resources/views/frontend/detail.blade.php
@@ -1,30 +1,12 @@
 @extends('ftontend.master')
 @section('title','Chi tiết sản phẩm')
 @section('main')
+		
+
+	<!-- Product Detail -->
 	<section class="sec-product-detail bg0 p-t-65 p-b-60">
 		<div class="container">
 			<div class="row">
-				<?php foreach ($products as $product) : ?>
-					<span 
-					data-code="<?php echo $product['maSP_morong']; ?>"
-					data-id="<?php echo $product['id']; ?>" 
-					data-price="<?php echo $product['gia']; ?>" 
-					data-discount-price="<?php echo $product['giamgia']; ?>"
-					data-stock="<?php echo $product['tonkho']; ?>"
-					>
-					</span>
-				<?php endforeach; ?>
-				<?php foreach ($dependencyData as $key => $dependencies) : ?>
-					<?php foreach ($dependencies as $dependency) : ?>
-					<span
-					data-color-id="<?php echo $key; ?>"
-					data-size-id="<?php echo $dependency['id']; ?>"
-					data-size-name="<?php echo $dependency['name']; ?>"
-					data-size-code="<?php echo $dependency['code']; ?>"
-					>
-					</span>
-					<?php endforeach; ?>
-				<?php endforeach; ?>
 				<div class="col-md-6 col-lg-7 p-b-30">
 					<div class="p-l-25 p-r-30 p-lr-0-lg">
 						<div class="wrap-slick3 flex-sb flex-w">
@@ -32,17 +14,35 @@
 							<div class="wrap-slick3-arrows flex-sb-m flex-w"></div>
 
 							<div class="slick3 gallery-lb">
-								<?php foreach ($productImages as $productImage): ?>
-								<div class="item-slick3" data-thumb="<?php echo PRODUCT_IMAGE_PATH . $productImage['tenhinh']; ?>">
+								<div class="item-slick3" data-thumb="images/product-detail-01.jpg">
 									<div class="wrap-pic-w pos-relative">
-										<img src="<?php echo PRODUCT_IMAGE_PATH . $productImage['tenhinh']; ?>" alt="IMG-PRODUCT">
+										<img src="images/product-detail-01.jpg" alt="IMG-PRODUCT">
 
-										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="<?php echo PRODUCT_IMAGE_PATH . $productImage['tenhinh']; ?>">
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-01.jpg">
 											<i class="fa fa-expand"></i>
 										</a>
 									</div>
 								</div>
-								<?php endforeach; ?>
+
+								<div class="item-slick3" data-thumb="images/product-detail-02.jpg">
+									<div class="wrap-pic-w pos-relative">
+										<img src="images/product-detail-02.jpg" alt="IMG-PRODUCT">
+
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-02.jpg">
+											<i class="fa fa-expand"></i>
+										</a>
+									</div>
+								</div>
+
+								<div class="item-slick3" data-thumb="images/product-detail-03.jpg">
+									<div class="wrap-pic-w pos-relative">
+										<img src="images/product-detail-03.jpg" alt="IMG-PRODUCT">
+
+										<a class="flex-c-m size-108 how-pos1 bor0 fs-16 cl10 bg0 hov-btn3 trans-04" href="images/product-detail-03.jpg">
+											<i class="fa fa-expand"></i>
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -51,47 +51,19 @@
 				<div class="col-md-6 col-lg-5 p-b-30">
 					<div class="p-r-50 p-t-5 p-lr-0-lg">
 						<h4 class="mtext-105 cl2 js-name-detail p-b-14">
-							<?php echo $products[0]['ten']; ?>
+							Lightweight Jacket
 						</h4>
-						
-						<div id="product-price">
-							<?php if (!empty($products[0]['giamgia']) && $products[0]['giamgia'] < $products[0]['gia']) : ?>
-								<span class="mtext-106 cl2" style="text-decoration: line-through; padding-right: 20px;">
-									<?php echo $db->formatPrice($products[0]['gia']); ?>
-								</span>
-								<span class="mtext-106 cl2" style="color: red;">
-									<?php echo $db->formatPrice($products[0]['giamgia']); ?>
-								</span>
-							<?php else: ?>
-								<span class="mtext-106 cl2">
-								<?php echo $db->formatPrice($products[0]['gia']); ?>
-								</span>
-							<?php endif; ?>
-						</div>
 
-						<p class="stext-102 cl3 p-t-23" id="product-code">
-							<?php echo $products[0]['maSP_morong']; ?>
+						<span class="mtext-106 cl2">
+							$58.79
+						</span>
+
+						<p class="stext-102 cl3 p-t-23">
+							Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.
 						</p>
-						<form action="index.php?view=product_detail&action=add-to-cart" method="POST">
+						
 						<!--  -->
 						<div class="p-t-33">
-							<div class="flex-w flex-r-m p-b-10">
-								<div class="size-203 flex-c-m respon6">
-									Color
-								</div>
-
-								<div class="size-204 respon6-next">
-									<div class="rs1-select2 bor8 bg0">
-										<select class="js-select2" name="color" onchange="getColorData(this);" required="">
-											<option value="">Choose an option</option>
-											<?php foreach ($colors as $color): ?>
-											<option value="<?php echo $color['id']; ?>" data-code="<?php echo $color['madactinh'] ?>"><?php echo $color['ten']; ?></option>
-											<?php endforeach; ?>
-										</select>
-										<div class="dropDownSelect2"></div>
-									</div>
-								</div>
-							</div>
 							<div class="flex-w flex-r-m p-b-10">
 								<div class="size-203 flex-c-m respon6">
 									Size
@@ -99,11 +71,31 @@
 
 								<div class="size-204 respon6-next">
 									<div class="rs1-select2 bor8 bg0">
-										<select class="js-select2" name="size" onchange="getSizeData(this);" required="">
-											<option value="">Choose an option</option>
-											<?php foreach ($sizes as $size): ?>
-											<option value="<?php echo $size['id']; ?>" data-code="<?php echo $size['madactinh'] ?>"><?php echo $size['ten']; ?></option>
-											<?php endforeach; ?>
+										<select class="js-select2" name="time">
+											<option>Choose an option</option>
+											<option>Size S</option>
+											<option>Size M</option>
+											<option>Size L</option>
+											<option>Size XL</option>
+										</select>
+										<div class="dropDownSelect2"></div>
+									</div>
+								</div>
+							</div>
+
+							<div class="flex-w flex-r-m p-b-10">
+								<div class="size-203 flex-c-m respon6">
+									Color
+								</div>
+
+								<div class="size-204 respon6-next">
+									<div class="rs1-select2 bor8 bg0">
+										<select class="js-select2" name="time">
+											<option>Choose an option</option>
+											<option>Red</option>
+											<option>Blue</option>
+											<option>White</option>
+											<option>Grey</option>
 										</select>
 										<div class="dropDownSelect2"></div>
 									</div>
@@ -112,30 +104,24 @@
 
 							<div class="flex-w flex-r-m p-b-10">
 								<div class="size-204 flex-w flex-m respon6-next">
-									<div id="quantity" class="wrap-num-product flex-w m-r-20 m-tb-10">
+									<div class="wrap-num-product flex-w m-r-20 m-tb-10">
 										<div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
 											<i class="fs-16 zmdi zmdi-minus"></i>
 										</div>
 
-										<input class="mtext-104 cl3 txt-center num-product" type="number" name="quantity" value="1">
+										<input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product" value="1">
 
 										<div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
 											<i class="fs-16 zmdi zmdi-plus"></i>
 										</div>
 									</div>
-									<button id="add-to-cart" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail" onclick="checkAddToCart(this);">
+
+									<button class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail">
 										Add to cart
-									</button>
-									<button id="sold-out" disabled="" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04 js-addcart-detail" style="display: none;">
-										Sold out
 									</button>
 								</div>
 							</div>	
 						</div>
-						<input type="hidden" name="product_code" value="<?php echo $products[0]['masanpham']; ?>">
-						<input type="hidden" name="product_id" value="<?php echo $products[0]['id_sanpham']; ?>">
-						<input type="hidden" name="main_product_id" value="<?php echo $products[0]['id']; ?>">
-						</form>
 
 						<!--  -->
 						<div class="flex-w flex-m p-l-100 p-t-40 respon7">
@@ -171,6 +157,10 @@
 						</li>
 
 						<li class="nav-item p-b-10">
+							<a class="nav-link" data-toggle="tab" href="#information" role="tab">Additional information</a>
+						</li>
+
+						<li class="nav-item p-b-10">
 							<a class="nav-link" data-toggle="tab" href="#reviews" role="tab">Reviews (1)</a>
 						</li>
 					</ul>
@@ -181,8 +171,67 @@
 						<div class="tab-pane fade show active" id="description" role="tabpanel">
 							<div class="how-pos2 p-lr-15-md">
 								<p class="stext-102 cl6">
-									<?php echo $products[0]['gioithieu'] ?>
+									Aenean sit amet gravida nisi. Nam fermentum est felis, quis feugiat nunc fringilla sit amet. Ut in blandit ipsum. Quisque luctus dui at ante aliquet, in hendrerit lectus interdum. Morbi elementum sapien rhoncus pretium maximus. Nulla lectus enim, cursus et elementum sed, sodales vitae eros. Ut ex quam, porta consequat interdum in, faucibus eu velit. Quisque rhoncus ex ac libero varius molestie. Aenean tempor sit amet orci nec iaculis. Cras sit amet nulla libero. Curabitur dignissim, nunc nec laoreet consequat, purus nunc porta lacus, vel efficitur tellus augue in ipsum. Cras in arcu sed metus rutrum iaculis. Nulla non tempor erat. Duis in egestas nunc.
 								</p>
+							</div>
+						</div>
+
+						<!-- - -->
+						<div class="tab-pane fade" id="information" role="tabpanel">
+							<div class="row">
+								<div class="col-sm-10 col-md-8 col-lg-6 m-lr-auto">
+									<ul class="p-lr-28 p-lr-15-sm">
+										<li class="flex-w flex-t p-b-7">
+											<span class="stext-102 cl3 size-205">
+												Weight
+											</span>
+
+											<span class="stext-102 cl6 size-206">
+												0.79 kg
+											</span>
+										</li>
+
+										<li class="flex-w flex-t p-b-7">
+											<span class="stext-102 cl3 size-205">
+												Dimensions
+											</span>
+
+											<span class="stext-102 cl6 size-206">
+												110 x 33 x 100 cm
+											</span>
+										</li>
+
+										<li class="flex-w flex-t p-b-7">
+											<span class="stext-102 cl3 size-205">
+												Materials
+											</span>
+
+											<span class="stext-102 cl6 size-206">
+												60% cotton
+											</span>
+										</li>
+
+										<li class="flex-w flex-t p-b-7">
+											<span class="stext-102 cl3 size-205">
+												Color
+											</span>
+
+											<span class="stext-102 cl6 size-206">
+												Black, Blue, Grey, Green, Red, White
+											</span>
+										</li>
+
+										<li class="flex-w flex-t p-b-7">
+											<span class="stext-102 cl3 size-205">
+												Size
+											</span>
+
+											<span class="stext-102 cl6 size-206">
+												XL, L, M, S
+											</span>
+										</li>
+									</ul>
+								</div>
 							</div>
 						</div>
 
@@ -194,7 +243,7 @@
 										<!-- Review -->
 										<div class="flex-w flex-t p-b-68">
 											<div class="wrap-pic-s size-109 bor0 of-hidden m-r-18 m-t-6">
-												<img src="../LayoutWeb/images/avatar-01.jpg" alt="AVATAR">
+												<img src="images/avatar-01.jpg" alt="AVATAR">
 											</div>
 
 											<div class="size-207">
@@ -283,69 +332,5 @@
 			</span>
 		</div>
 	</section>
-
-<script type="text/javascript">
-	function getColorData(el) {
-		var colorId = $(el).val();
-		var sizeData = $('span[data-color-id="' + colorId + '"]');
-		var template = '<option value="">Choose an option</option>';
-		$.each(sizeData, function(idx, el){
-			var sizeId = $(el).attr('data-size-id');
-			var sizeName = $(el).attr('data-size-name');
-			var sizeCode = $(el).attr('data-size-code');
-			template += '<option value="' + sizeId + '" data-code="' + sizeCode +'">' + sizeName + '</option>';
-		});
-
-		$('select[name="size"]').html(template);
-	}
-	function getSizeData(el) {
-		var productCode = $('form input[name="product_code"]').val();
-		var sizeCode = $(el).find('option:selected').attr('data-code');
-		var colorCode = $('select[name="color"]').find('option:selected').attr('data-code');
-		var mainCode = productCode + '-' + sizeCode + '-' + colorCode;
-		var price = $('span[data-code="' + mainCode + '"]').attr('data-price');
-		var discountPrice = $('span[data-code="' + mainCode + '"]').attr('data-discount-price');
-		var stock = $('span[data-code="' + mainCode + '"]').attr('data-stock');
-		var mainId = $('span[data-code="' + mainCode + '"]').attr('data-id');
-		var priceTemplate = '';
-
-		if (parseFloat(price) - parseFloat(discountPrice) > 0) {
-			priceTemplate += '<span class="mtext-106 cl2" style="text-decoration: line-through; padding-right: 20px;">'
-							+ formatPrice(price) + '</span>'
-							+ '<span class="mtext-106 cl2" style="color: red;">'
-							+ formatPrice(discountPrice) + '</span>'
-			;
-		} else {
-			priceTemplate += '<span class="mtext-106 cl2">' + formatPrice(price) + '</span>';	
-		}
-
-		if (parseInt(stock) > 0) {
-			$('#quantity').show();
-			$('button#add-to-cart').show();
-			$('button#sold-out').hide();
-			$('input[name="quantity"]').attr('max', stock);
-		} else {
-			$('#quantity').hide();
-			$('button#sold-out').show();
-			$('button#add-to-cart').hide();
-		}
-
-		$('div#product-price').html(priceTemplate);
-		$('p#product-code').html(mainCode);
-		$('input[name="main_product_id"]').val(mainId);
-	}
-
-	function checkAddToCart(el) {
-		var color = $('select[name="color"]').val();
-		var size = $('select[name="size"]').val();
-
-		if (size == null || color == null) {
-			return false;
-		}
-
-		return true;
-	}
-
-</script>
 
 @stop
